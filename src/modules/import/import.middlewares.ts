c5b60672f67a4f1b9c9801de86de48839f104d27
awenn2015 <44868028+relaxdd@users.noreply.github.com>
2023-11-11 03:02:51 +0400

    Update import.middlewares.ts

--- src/modules/import/import.middlewares.ts
+++ src/modules/import/import.middlewares.ts
@@ -1,16 +1,16 @@
-import multer from "multer"
-import path from "path"
-import crypto from "crypto"
-import { celebrate } from "celebrate"
-import { tableIdSchema } from "./import.scheme"
+import multer from 'multer'
+import path from 'path'
+import crypto from 'crypto'
+import { celebrate } from 'celebrate'
+import { tableIdSchema } from './import.scheme'
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/")
+    cb(null, path.join(process.cwd(), 'uploads'))
   },
   filename: (req, file, cb) => {
     crypto.randomBytes(16, function (err, raw) {
-      cb(err, raw.toString("hex") + path.extname(file.originalname))
+      cb(err, raw.toString('hex') + path.extname(file.originalname))
     })
   },
 })
@@ -18,8 +18,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 export const multerFilesReceiver = upload.fields([
-  { name: "table", maxCount: 1 },
-  { name: "options", maxCount: 1 },
+  { name: 'table', maxCount: 1 },
+  { name: 'options', maxCount: 1 },
 ])
 
 export const validateTableId = celebrate({
